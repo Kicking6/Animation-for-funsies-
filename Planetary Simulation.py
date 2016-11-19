b9b2da82bad778f3775e3d1b47d95ce955be1b68
James Brown <jamesphone180@gmail.com>
2016-11-19 15:38:36 +1300

    Changed the definitions of the data structures.
    Fixed the mission mass paramater and re-ordered the simulation
    variables, also moved the state to a global variable.

--- Planetary Simulation.py
+++ Planetary Simulation.py
@@ -1,34 +1,35 @@
-#Defines the starting conditions for the start of the simulation
-#Returns a list of tupples where each tupple contains (eccentricity, starting angle, apogee, perigee)
-def get_starting_conditions(file_name):
-    perigee = 12
-    apogee = 14
-    return [(0.9, 0, apogee, perigee)]
+#The list of all objects to be simulated in the form (position, velocity, standard gravitational paramater, radius)[]
+state = []
 
-#Takes a tupple (eccentricity, starting angle, apogee, perigee) and returns
-#a tupple continaing (position, mass, radius, velocity)
+#Defines the starting conditions for the start of the simulation
+#Returns a list of tupples where each tupple contains (mass, radius, starting angle, apogee, perigee)
+def get_starting_conditions(file_name):
+
+#Takes a tupple (mass, radius, starting angle, apogee, perigee) and returns
+#a tupple continaing (position, velocity, standard gravitational paramater, radius).
+#The orbital characteristics are defined relative to the center of mass of the whole system.
 def convert_to_internal_representation(thing_data):
 
 #Sets up the display objects for the animator
-def initialise_display(things):
+def initialise_display():
 
 #Updates the display objects
-def display(things):
-    "Does stuff"
-    #TODO
+def display():
 
 #Steps the things forward by timestep
-def simulate(timestep, things):
-    #TODO
+def simulate(timestep):
 
 #Displays the window and starts the animation, this will also start the phyiscal simulation
-def show_simulation(things):
+def show_simulation():
 
 def main():
     filename = input("Enter the filename for the initial starting conditions:")
-    conditions = [convert_to_internal_representation(thing) for thing in get_starting_conditions(filename)]
+    state = [convert_to_internal_representation(thing) for thing in get_starting_conditions(filename)]
+
+    #FIX: This may need to be made global, atm it is unused.
     dt = input("Timestep:")
-    initialise_display(conditions)
-    show_simulation(conditions)
+    
+    initialise_display()
+    show_simulation()
 
 main()
