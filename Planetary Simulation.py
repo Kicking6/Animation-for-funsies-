5c35002595ba0953adb737a139837e61b83acbbb
James Brown <jamesphone180@gmail.com>
2016-11-19 17:29:56 +1300

    Cleaned up orbital definitions

--- Planetary Simulation.py
+++ Planetary Simulation.py
@@ -2,13 +2,15 @@
 state = []
 
 #Defines the starting conditions for the start of the simulation
-#Returns a list of tupples where each tupple contains (mass, radius, starting angle, apogee, perigee)
+#Returns a list of tupples where each tupple contains (parent_body, mass, radius, true_anomaly, apsis, periapsis, argument_of_periapsis)
+#A starting angle of 0 indicates that the body is directly above (+y). Anti-clockwise, in radians.
 def get_starting_conditions(file_name):
 
-#Takes a tupple (mass, radius, starting angle, apogee, perigee) and returns
+#Takes a list of tupples (parent_body, mass, radius, true_anomaly, apsis, periapsis, argument_of_periapsis) and returns
 #a tupple continaing (position, velocity, standard gravitational paramater, radius).
 #The orbital characteristics are defined relative to the center of mass of the whole system.
-def convert_to_internal_representation(thing_data):
+#If the mass is -1 then the body is a ficticious body only defined to define the starting conditions.
+def convert_to_internal_representation(initial_state):
 
 #Sets up the display objects for the animator
 def initialise_display():
@@ -24,7 +26,8 @@ def show_simulation():
 
 def main():
     filename = input("Enter the filename for the initial starting conditions:")
-    state = [convert_to_internal_representation(thing) for thing in get_starting_conditions(filename)]
+    
+    state = convert_to_internal_representation(get_starting_conditions(filename))
 
     #FIX: This may need to be made global, atm it is unused.
     dt = input("Timestep:")
