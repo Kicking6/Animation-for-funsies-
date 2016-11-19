4e7be56b74cad3727a7ab4b63857b0b89bb7331b
Kicking6 <rorywadeallen@gmail.com>
2016-11-19 23:48:06 +1300

    Completed get starting conditions
    Changed from using named tuples to simple names spaces, ie they are not
    simple

--- Planetary Simulation.py
+++ Planetary Simulation.py
@@ -1,22 +1,43 @@
-import collections
-
-State = collections.namedtuple('State', ['position', 'velocity', 'SG', 'radius'])
-Vector = collections.namedtuple('Vector', ['x', 'y', 'z'])
-InitialState = collections.namedtuple('InitialState', ['parent_body', 'mass', 'radius', 'true_anomaly', 'apsis', 'periapsis', 'longitude_of_periapsis'])
+import types
+import yaml
 
 #The list of all objects to be simulated in the form (position, velocity, standard gravitational paramater, radius)[]
 state = []
 
 #Defines the starting conditions for the start of the simulation
-#Returns a list of tupples where each tupple contains (parent_body, mass, radius, true_anomaly, apsis, periapsis, longitude_of_periapsis)
+#Returns a dictionary of dictionaires where each tupple contains (parent_body, mass, radius, true_anomaly, apsis, periapsis, longitude_of_periapsis)
 #A starting angle of 0 indicates that the body is directly above (+y). Anti-clockwise, in radians.
 def get_starting_conditions(file_name):
-	file = open(file_name, "r")
-	contents = file.read()
-	file.close()
-	#Not sure how to extract this information from the file, I have only ever split on white space etc from a .txt
-    print('Not done')
-    return []
+    file_name = 'Starting Conditions.yaml'
+    file = open(file_name, "r")
+    contents = yaml.load(file)
+
+    object_list = []
+    place_dict = {}
+    place = 0
+    for i in contents:
+        name = i['name'] 
+        place_dict[name] = place
+        place += 1
+
+    for i in contents:
+        initial_state = types.SimpleNamespace()
+        initial_state.name = i['name']
+        initial_state.parent_body = i['parent_body']
+        initial_state.mass = i['mass']
+        initial_state.radius = i['radius']
+        initial_state.true_anomaly = i['true_anomaly']
+        initial_state.apsis = i['apsis']
+        initial_state.periapsis = i['periapsis']
+        initial_state.longitude_of_periapsis = i['longitude_of_periapsis']
+        object_list.append(initial_state)
+    
+        key_to_find = i['parent_body']
+    
+        if key_to_find in place_dict:
+            initial_state.parent_body = place_dict[key_to_find]
+    
+    return [object_list]
 
 #Takes a list of tupples (parent_body, mass, radius, true_anomaly, apsis, periapsis, longitude_of_periapsis) and returns
 #a tupple continaing (position, velocity, standard gravitational paramater, radius).
@@ -43,7 +64,7 @@ def show_simulation():
     print('Not done')
     
 def main():
-    filename = input("Enter the filename for the initial starting conditions:")
+    filename = input("Enter the filename for the initial starting conditions: ")
     
     state = convert_to_internal_representation(get_starting_conditions(filename))
 
